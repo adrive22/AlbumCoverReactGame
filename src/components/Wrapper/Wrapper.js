4612f6349774fcd85bf031e16b52a148c746f102
Allison Driver <33002895+adrive22@users.noreply.github.com>
2024-04-03 14:05:07 -0500

    Delete src directory

--- src/components/Wrapper/Wrapper.js
+++
@@ -1,7 +0,0 @@
-//method/component for getting accessing the children property and inputing it into the html
-
-import React from "react";
-
-const Wrapper = props => <div className="wrapper">{props.children}</div>;
-
-export default Wrapper;
